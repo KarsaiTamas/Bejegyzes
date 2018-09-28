0edd707824bd98fd85104ed2a1626aaf0a21f643
Karsai Tamás <karsai106@gmail.com>
2018-09-28 13:51:28 +0200

    Kezd

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        string szerzo;
+        string tartalom;
+        int likeok;
+        DateTime letrejott;
+        DateTime szerkesztve;
+
+    }
+}
