9ea86b0cb16302b9d4f51cf45ac9c889690dc22d
Karsai Tamás <karsai106@gmail.com>
2018-09-28 15:00:27 +0200

    Ameddig jutottam

--- BejegyzesProjekt/Bejegyzes.cs
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -8,11 +8,116 @@ namespace BejegyzesProjekt
 {
     class Bejegyzes
     {
-        string szerzo;
-        string tartalom;
-        int likeok;
-        DateTime letrejott;
-        DateTime szerkesztve;
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+
+        public Bejegyzes(string szerzo,string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            letrejott = DateTime.Now;
+            szerkesztve = DateTime.Now;
+        }
+        public string Tartalom
+        {
+            get
+            {
+                return tartalom;
+            }
+            set
+            {
+                this.tartalom = value;
+                letrejott = DateTime.Now;
+            }
+        }
+
+        public string Szerzo
+        {
+            get
+            {
+                return szerzo;
+            }
+
+           
+        }
+           
+        public int Likeok
+        {
+            get { return likeok; }
+        }
+
+        public DateTime Letrejott{
+
+            get {
+                return letrejott;
+            }
+
+            }
+
+        public DateTime Szerkesztve
+        {
+
+            get
+            {
+                return szerkesztve;
+            }
+
+        }
+
+        public void Like()
+        {
+            this.likeok++;
+        }
+
+        public string Kiir()
+        {
+            string vissza="";
+            vissza = this.Szerzo +
+                " - " + this.Likeok +
+                " - " + this.Letrejott +
+                "\nSzerkesztve: " +
+                this.Szerkesztve +
+                "\n" + this.Tartalom;
+
+
+
+
+            return vissza;
+        }
+
+        public override string ToString()
+        {
+            string vissza = "";
+
+            if (this.Letrejott!=this.Szerkesztve)
+            {
+                
+                vissza = this.Szerzo +
+                    " - " + this.Likeok +
+                    " - " + this.Letrejott +
+                    "\n" + this.Tartalom;
+            }
+            else
+            {
+                 
+                vissza = this.Szerzo +
+                    " - " + this.Likeok +
+                    " - " + this.Letrejott +
+                    "\nSzerkesztve: " +
+                    this.Szerkesztve +
+                    "\n" + this.Tartalom;
+            }
+
+
+            return vissza;
+        }
+
+
 
     }
 }
--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -3,13 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace BejegyzesProjekt
 {
     class Program
     {
+        static void Szoveg_Textbol(string helye)
+        {
+            StreamReader olvas = new StreamReader(helye);
+            while (!olvas.EndOfStream)
+            {
+                string sor=olvas.ReadLine();
+                string[] adatok = sor.Split(';');
+                bejegyez_lista.Add(new Bejegyzes(adatok[0], adatok[1]));
+            }
+
+            olvas.Close();
+        }
+
+        private static List<Bejegyzes> bejegyez_lista = new List<Bejegyzes>();
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            Bejegyzes jegyez = new Bejegyzes("sanyi", "jóska pista megcsinálta");
+            Console.WriteLine(jegyez);
+            Console.WriteLine();
+            
+            bejegyez_lista.Add(new Bejegyzes("jóska", "a disznó büdi"));
+            bejegyez_lista.Add(new Bejegyzes("pista", "a sövény nagyranőtt"));
+            foreach (var item in bejegyez_lista)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            int darab_szam;
+            string beker = "";
+            do
+            {
+                Console.Write("Hány bejegyzést szeretne hozzáadni: ");
+                 beker = Console.ReadLine();
+
+
+            } while (int.TryParse(beker,out darab_szam)==false);
+
+            int a = darab_szam;
+            if (darab_szam<0)
+            {
+                a = 0;
+            }
+
+            do
+            {
+                if (a > 0)
+                {
+                    Console.WriteLine("Kérek egy szerzot");
+                    string szerzo = Console.ReadLine();
+                    Console.WriteLine("Kérem a tartalmat");
+                    string tartalom = Console.ReadLine();
+                    bejegyez_lista.Add(new Bejegyzes(szerzo, tartalom));
+                    a--;
+                }
+            } while (a>0);
+            Szoveg_Textbol("bejegyzesek.txt");
+            foreach (var item in bejegyez_lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < bejegyez_lista.Count*20; i++)
+            {
+                int szam = rnd.Next(0, bejegyez_lista.Count);
+                bejegyez_lista[szam].Like();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Kérem változtassa meg a 2. bejegyzest");
+            bejegyez_lista[1].Tartalom = Console.ReadLine();
+
+
+            foreach (var item in bejegyez_lista)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
